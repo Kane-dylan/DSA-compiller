d79125a3da5a63777932c2cefd4673029bb836c3
Kane Dylan <cool.kdtech1234@gmail.com>
2025-07-12 18:17:59 +0530

    Dsa update

---
+++ DSA/Demo.java
@@ -0,0 +1,11 @@
+package DSA;
+import java.util.Scanner;
+
+public class Demo{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int x =sc.nextInt();
+    System.out.println("Hey"+x);
+    sc.close();
+  }
+}
---
+++ DSA/Test.java
@@ -0,0 +1,18 @@
+package DSA;
+
+import java.util.Scanner;
+
+public class Test{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int x= sc.nextInt();
+
+    int cnt=0;
+    while(x!=0){
+      int LD=x%10;
+      cnt++;
+      x /= 10;
+    }
+    System.out.println(cnt);
+  }
+}
