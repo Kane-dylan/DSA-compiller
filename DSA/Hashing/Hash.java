89b11dd212ac7712612766ad9d6b964ee9a838b5
Kane Dylan <cool.kdtech1234@gmail.com>
2025-07-14 23:42:32 +0530

    hashing 1/5

---
+++ DSA/Hashing/Hash.java
@@ -0,0 +1,31 @@
+package DSA.Hashing;
+
+import java.util.Scanner;
+
+public class Hash {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int size = sc.nextInt();
+    int arr[] = new int[size];
+    // input
+    for (int i = 0; i < size; i++) {
+      arr[i] = sc.nextInt();
+    }
+
+    // precompute
+    int hash[] = new int[13];
+    for (int i = 0; i < size; i++) {
+      hash[arr[i]] += 1;
+    }
+
+    // output
+    int q = sc.nextInt();
+    while (q>0) {
+      int num = sc.nextInt();
+      // fetch
+      System.out.println(hash[num]);
+      q--;
+    }
+  }
+}
